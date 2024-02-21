c435d40e5c77cf5d60b4f92c4434d222fda04556
Mariana Salinas Camus <150278041+mariana-sc@users.noreply.github.com>
2024-02-21 13:07:26 +0100

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import copy
+
+
+def add_remaining_useful_life(df):
+    """
+    Add the remaining useful life to the dataframe
+    :param df: Dataframes from the CMAPSS dataset
+    :return: Dataframe with the remaining useful life added
+    """
+    # Get the total number of cycles for each unit
+    grouped_by_unit = df.groupby(by="unit_nr")
+    max_cycle = grouped_by_unit["time_cycles"].max()
+
+    # Merge the max cycle back into the original frame
+    result_frame = df.merge(max_cycle.to_frame(name='max_cycle'), left_on='unit_nr', right_index=True)
+
+    # Calculate remaining useful life for each row
+    remaining_useful_life = result_frame["max_cycle"] - result_frame["time_cycles"]
+    result_frame["RUL"] = remaining_useful_life
+
+    # drop max_cycle as it's no longer needed
+    result_frame = result_frame.drop("max_cycle", axis=1)
+    return result_frame
+
+
+def create_dataset(df):
+    """
+    Create a dataset from the dataframe from the principal component analysis
+    :param df: pca_df
+    :return: Dataset for the clustering analysis
+    """
+    return [np.array([pc1, pc2]) for pc1, pc2 in zip(df['Principal Component 1'], df['Principal Component 2'])]
+
+
+def get_results(clusters, dataset):
+    """
+    Get the results from the clustering analysis in the correct format for silhouette_score and plot_results
+    :param clusters: Results from the clustering analysis
+    :param dataset: Input dataset
+    :return:
+    """
+    labels = copy.deepcopy(dataset)
+    for i in range(len(clusters)):
+        for idx in range(len(dataset)):
+            if idx in clusters[i]:
+                labels[idx] = i
+    return labels
+
+
+def plot_results(pca_df, clusters, centers):
+    """
+    Plot the results from the clustering analysis
+    :param pca_df: pca_df
+    :param clusters: Results from the clustering analysis
+    :param centers: Cluster centers as calculated by the clustering algorithm
+    :return:
+    """
+
+    # Prepare the data
+    plt.figure(figsize=(10, 6))
+
+    dataset = create_dataset(pca_df)
+    clusters_res = get_results(clusters, dataset)
+
+    results_man_df = copy.deepcopy(pca_df)
+    results_man_df['clusters'] = clusters_res
+    results_df_1 = results_man_df[results_man_df['dataset'] == 1]
+    results_df_3 = results_man_df[results_man_df['dataset'] == 3]
+
+    # Plot data points for each dataset
+    plt.scatter(results_df_1['Principal Component 1'], results_df_1['Principal Component 2'], marker="s",
+                c=results_df_1['clusters'], cmap='plasma', edgecolors='k', s=120, alpha=0.8, label='Dataset 001')
+
+    plt.scatter(results_df_3['Principal Component 1'], results_df_3['Principal Component 2'], marker="*",
+                c=results_df_3['clusters'], cmap='plasma', edgecolors='k', s=100, alpha=0.8, label='Dataset 003')
+
+    # Plot cluster centers
+    centers = np.array(centers)
+    plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='X', s=200, label='Cluster Centers')
+
+    plt.title('K-means Clustering after PCA')
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.grid(visible=False)
+    plt.legend()
+    plt.show()
